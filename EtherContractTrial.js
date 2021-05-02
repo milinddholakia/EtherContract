c3481238caae12b396b3d4e3f98f74de38fa0975
milinddholakia <milinddholakia0@gmail.com>
2021-05-03 00:28:27 +0530

    Reading Contract and get API

---
+++ EtherContractTrial.js
@@ -0,0 +1,35 @@
+const { ethers } = require("ethers");
+const express = require("express");
+const app =express();
+const provider = ethers.getDefaultProvider('ropsten');
+
+// const signer = provider.getSigner()
+
+const contractAddress = "0x4c96Ac9436E3b32a5E004B79eBa2f09853fc7D91";
+const contractAbi =[ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "A", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "B", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "contestant", "type": "string" } ], "name": "Vote", "outputs": [ { "internalType": "string", "name": "s", "type": "string" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "check", "outputs": [ { "internalType": "address", "name": "person", "type": "address" }, { "internalType": "string", "name": "voteGiven", "type": "string" } ], "stateMutability": "view", "type": "function" } ];
+const connectContract =  new ethers.Contract(contractAddress,contractAbi, provider);
+
+
+async function candidateA(){ 
+const valueA = await connectContract.A();
+console.log(valueA.toString());
+}
+async function candidateB(){ 
+
+const valueB =await connectContract.B();
+console.log(valueB.toString());
+
+// const connectWriteContract = new ethers.Contract(contractAddress,contractAbi, signer);
+// const tx= await connectWriteContract.Vote("A");
+// const txResponse =await tx.wait();
+// console.log(txResponse.toString());
+
+}
+app.get('/getCandidateA',(req,res)=>{
+    res.send(candidateA().toString);
+});
+app.get('/getCandidateB',(req,res)=>{
+    res.send(candidateB());
+});
+
+app.listen(3000,()=>console.log('Lets Begin'));
